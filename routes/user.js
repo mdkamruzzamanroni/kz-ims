791acb2cac9233f4691463ad93601144dabfcff4
Md Kamruzzaman <kamruzzaman.dte@gmail.com>
2022-10-30 23:04:02 +0600

    server run success

---
+++ routes/user.js
@@ -0,0 +1,9 @@
+//const { Router } = require("express");//module.exports=Router; or below both r correct
+
+
+const express=require("express");
+const router= express.Router();  //(TypeError: express.router is not a function)
+
+
+
+module.exports=router;
--- server.js
+++ server.js
@@ -13,7 +13,8 @@ app.use(helmet());
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
-
+app.use(morgan("dev"));
+app.use(cors());
 
 //DB connection
 mongoose
@@ -22,11 +23,11 @@ mongoose
     .catch((err)=>console.log("DB Error=>",err));
 
 //routes middleware    (readdirSync("./routes")this portion will return an:ARRAY)
-readdirSync("./routes").map(r=>app.use("api/v1",require('./routes/${r}')));
+
+readdirSync("./routes").map(r=>app.use("/api/v1",require(`./routes/${r}`)));
 
 //server
-const port=process.env.port || 8000;
-
+const port=process.env.PORT || 8000;
 
 app.listen(port,() => {
     console.log(`App is  running on port ${port}`);
